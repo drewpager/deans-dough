b1514a3099755be44d90b126206c3dd05c8e5627
Drew Page <drew@siegemedia.com>
2020-10-12 19:24:24 -0700

    initial commit

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: `Dean's Dough`,
+    siteUrl: 'http://deansdough.com',
+    description: `The best dough in Dean's world`,
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '9l4muuxj',
+        dataset: 'production',
+        watchmode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
